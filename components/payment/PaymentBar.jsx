fc7ad0dbf3ad235a2eb96606dac071731cf73cdf
Mihaela Vlad <vlad.c.mihaela@gmail.com>
2022-07-15 14:35:53 +0300

    active links

--- components/payment/PaymentBar.jsx
+++ components/payment/PaymentBar.jsx
@@ -1,43 +1,31 @@
-import Link from 'next/link';
+import ActiveLink from './../ActiveLink';
 
 export default function PaymentBar() {
   return (
     <section className="payment-bar">
       <ul className="payment-bar-progress">
         <li>
-          <Link href="/order-sign-in">
-            <a title="Sign in">
-              Sign in
-              <span className="step first-step step-active"></span>
-            </a>
-          </Link>
+          <ActiveLink href="/order-sign-in" activeClassName="active">
+            <a title="Sign in">Sign in</a>
+          </ActiveLink>
         </li>
 
         <li>
-          <Link href="/shipping">
-            <a title="Shipping">
-              Shipping
-              <span className="step second-step step-inactive"></span>
-            </a>
-          </Link>
+          <ActiveLink href="/shipping" activeClassName="active">
+            <a title="Shipping">Shipping</a>
+          </ActiveLink>
         </li>
 
         <li>
-          <Link href="/billing">
-            <a title="Billing">
-              Billing
-              <span className="step third-step step-inactive"></span>
-            </a>
-          </Link>
+          <ActiveLink href="/billing" activeClassName="active">
+            <a title="Billing">Billing</a>
+          </ActiveLink>
         </li>
 
         <li>
-          <Link href="/confirm">
-            <a title="Confirm">
-              Confirm
-              <span className="step fourth-step step-inactive"></span>
-            </a>
-          </Link>
+          <ActiveLink href="/confirm" activeClassName="active">
+            <a title="Confirm">Confirm</a>
+          </ActiveLink>
         </li>
       </ul>
     </section>
